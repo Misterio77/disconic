fdb1c3e7ef714af920b2dd7e18b363cc58ff0b0a
Gabriel Fontes <hi@m7.rs>
2023-06-11 13:38:15 -0300

    bump sunk to fix album id's

--- default.nix
+++ default.nix
@@ -13,7 +13,7 @@ in rustPlatform.buildRustPackage {
   cargoLock = {
     lockFile = ./Cargo.lock;
      outputHashes = {
-       "sunk-0.1.2" = "sha256-0kCNEb38NbrhfgFB+aj3vcpWZhWjfHBP7V/CjUrMkF4=";
+       "sunk-0.1.2" = "sha256-gxDinyzKHxph2D5OZ9TevVEWtsEnnFpu4BfkkBnRMx4=";
        "serenity-0.11.5" = "sha256-10s0kflNYEMwUXAgrh6d1IUk3ZRSCkAilz9m1lVhXhA=";
        "songbird-0.3.2" = "sha256-8wzCcV9W6K0MHqZ8yhTIMjh165NV8OQ9zlgrRrIhlOI=";
      };
