c7872b0ace9c2fc656b805910b24d94861bcd62d
Gabriel Fontes <hi@m7.rs>
2023-06-11 02:07:53 -0300

    switch to songbird next

--- flake.nix
+++ flake.nix
@@ -7,26 +7,40 @@
       [ "cache.m7.rs:kszZ/NSwE/TjhOcPPQ16IuUiuRSisdiIwhKZCxguaWg=" ];
   };
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, rust-overlay }:
     let
       forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" ];
       forAllPkgs = f: forAllSystems (sys: f pkgsFor.${sys});
-      pkgsFor = nixpkgs.legacyPackages;
+      pkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ rust-overlay.overlays.default ];
+      });
+      mkPackage = pkgs: pkgs.callPackage ./default.nix {
+        rustPlatform = pkgs.makeRustPlatform rec {
+          rustc = pkgs.rust-bin.stable.latest.default;
+          cargo = rustc;
+        };
+      };
     in {
       nixosModules.default = import ./module.nix;
 
       overlays.default = final: _prev: {
-        disconic = final.callPackage ./default.nix { };
+        disconic = mkPackage final;
       };
 
-      packages =
-        forAllPkgs (pkgs: { default = pkgs.callPackage ./default.nix { }; });
+      packages = forAllPkgs (pkgs: {
+        default = mkPackage pkgs;
+      });
 
       devShells = forAllPkgs (pkgs: {
         default = pkgs.mkShell {
-          inputsFrom = [ self.outputs.packages.${pkgs.system}.default ];
+          inputsFrom = [(mkPackage pkgs)];
           buildInputs = with pkgs; [ clippy rust-analyzer rustc rustfmt ffmpeg ];
         };
       });
